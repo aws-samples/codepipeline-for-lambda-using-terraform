433249a9ffac988d7d2d036cea38683a861827e3
Arun Jacob <116331545+ajeyasinghjacob@users.noreply.github.com>
2022-11-04 21:31:15 -0700

    updates to path

---
+++ lambda_bootstrap/providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform
+{
+    backend "s3" {
+      encrypt = true
+      bucket = "BUCKET_NAME"
+      key = "states/lambda/terraform.tfstate"
+      region = "us-east-1"
+  }
+}

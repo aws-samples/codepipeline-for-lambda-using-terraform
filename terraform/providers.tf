60065053daa9093c636b8b08c54c80a190be7054
Arun Jacob <116331545+ajeyasinghjacob@users.noreply.github.com>
2022-11-04 21:30:28 -0700

    update to path

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,7 +7,7 @@ terraform
     backend "s3" {
       encrypt = true
       bucket = "BUCKET_NAME"
-      key = "states/lambda/terraform.tfstate"
+      key = "states/code/terraform.tfstate"
       region = "us-east-1"
   }
 }
